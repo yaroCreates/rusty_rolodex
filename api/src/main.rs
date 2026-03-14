bdc2529ba841981df48909bff165a8bd9e1cab9c
Onuh James Yaro <onuhjamesyaro@gmail.com>
2026-03-14 16:15:29 +0100

    feat: api DELETE

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,8 +1,10 @@
-use std::{
-    sync::{Arc, Mutex},
-};
+use std::sync::{Arc, Mutex};
 
-use axum::{Json, Router, extract::State, routing::get};
+use axum::{
+    Json, Router,
+    extract::{Path, State},
+    routing::{ get, put},
+};
 use chrono::{DateTime, Utc};
 // use rusty_rolodex::{core::domain::AppState, domain::Contact, prelude::AppError};
 use rolodex_core::{
@@ -31,8 +33,7 @@ pub struct SpecialContact {
 pub struct ApiResponse {
     status: String,
     message: String,
-    data: Vec<Contact>
-
+    data: Option<Vec<Contact>>,
 }
 
 #[tokio::main]
@@ -43,6 +44,10 @@ async fn main() -> Result<(), AppError> {
     let app = Router::new()
         .route("/", get(|| async { "Hello, World!" }))
         .route("/contacts", get(get_contacts).post(post_contacts))
+        .route(
+            "/contacts/{contact_id}",
+            put(edit_contact).delete(delete_contact),
+        )
         .with_state(store);
 
     async fn get_contacts(State(state): State<Arc<Mutex<FileStore>>>) -> Json<Vec<Contact>> {
@@ -90,18 +95,50 @@ async fn main() -> Result<(), AppError> {
 
         println!("Axum payload after inserting:{:?}", payload);
 
-        let api_response = 
-            ApiResponse {
-                status: "success".to_string(),
-                message: "Contact created successfully".to_string(),
-                data: payload
-            };
-            
+        let api_response = ApiResponse {
+            status: "success".to_string(),
+            message: "Contact created successfully".to_string(),
+            data: Some(payload),
+        };
+
         Json(api_response)
-        
     }
 
+    async fn delete_contact(
+        State(state): State<Arc<Mutex<FileStore>>>,
+        Path(contact_id): Path<Uuid>,
+    ) -> Json<ApiResponse> {
+        //
+        let guard = state.lock().unwrap();
 
+        //Get the contacts
+        let mut contacts = guard.load().expect("Error occurred while getting contacts");
+
+        let result = contacts.remove(&contact_id);
+
+        let api_response: ApiResponse;
+
+        if let Some(response) = result {
+
+            guard.save(contacts).expect("Panic occured");
+
+            api_response = ApiResponse {
+                status: "success".to_string(),
+                message: format!("Contact with id:{} deleted!", contact_id),
+                data: Some(vec![response]),
+            };
+            Json(api_response)
+        } else {
+            api_response = ApiResponse {
+                status: "error".to_string(),
+                message: format!("Contact with id: {} not found", contact_id),
+                data: None,
+            };
+            Json(api_response)
+        }
+    }
+
+    async fn edit_contact() {}
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
